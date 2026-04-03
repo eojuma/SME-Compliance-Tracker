5ff391f9804a4b9a82241c80650915d7ab7da2f6
eojuma <evansjuma1e@gmail.com>
2026-04-03 06:21:51 +0300

    update main

--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,1 @@
 package main
-
-import (
-	"fmt"
-	"log"
-	"net/http"
-	"SME-compliance-tracker/routes"
-)
-
-func main() {
-	router := routes.SetupRoutes()
-	fmt.Println("Server running on http://localhoost:8080")
-	err := http.ListenAndServe(":8080", router)
-	if err != nil {
-		log.Fatal("Server failed: ", err)
-	}
-}
